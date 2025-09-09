b54946f42da59e8480aca49e2cc6e4ad67ff6547
BigFei <364950776@qq.com>
2025-09-09 11:01:29 +0800

    release: v0.0.1

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 clean:
 	rm -rf dist
 	rm -rf *.egg-info
+
+build:
+	uv python -m build
+
+upload:
+	uv run twine upload --username __token__ dist/* 
--- faster_app/settings/builtins/settings.py
+++ faster_app/settings/builtins/settings.py
@@ -11,7 +11,7 @@ class DefaultSettings(BaseSettings):
 
     # 基础配置
     PROJECT_NAME: str = "Faster API"
-    VERSION: str = "0.1.0"
+    VERSION: str = "0.0.1"
     DEBUG: bool = True
 
     # Server 配置
